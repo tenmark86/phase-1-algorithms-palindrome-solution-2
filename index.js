d3909cffd37c1343216beb7457b932054d1bac28
tenmark86e <tenmark86@gmail.com>
2022-08-24 14:02:01 +0300

    Done Palindrome Lab Solution 2 assignment

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   // iterate from the beginning of the string to the middle of the string
+   for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+  if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
